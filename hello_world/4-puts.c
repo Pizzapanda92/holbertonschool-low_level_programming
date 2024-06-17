84190744b15e017ba83d2f97d46683815f498ec6
Pizzapanda92 <fabio92@sfr.fr>
2024-06-17 13:04:42 +0000

    exo 4/0

---
+++ hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/*print test*/
+int main(void) 
+{
+
+    /* Notice that final new line character is implicit */
+    puts("\"Programming is like building a multilingual puzzle\n");       
+         return 0;
+{
