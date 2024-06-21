906ad120a41e06f00f95715369eb935e5f303c2f
Pizzapanda92 <fabio92@sfr.fr>
2024-06-21 12:54:49 +0000

    exo 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs -check if int will find an absolute
+ * @r: character
+ * Return: 0.
+ */
+
+int _abs(int r)
+{
+
+	if (r >= '0')
+	{
+		return (r);
+	}
+	else
+	{
+		return (-r);
+	}
+
+}
