eeee13743a7b58eefaea0fb47cde06c613c0afd3
Pizzapanda92 <fabio92@sfr.fr>
2024-06-21 09:24:25 +0000

    exo 3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * int _islower(int c) -function that checks for lowercase character.
+ *
+ * Return: void.
+ */
+
+
+int _islower(int c)
+
+{
+
+	if (islower(c))
+	{
+		return (1);
+	}
+	return (0);
+}
