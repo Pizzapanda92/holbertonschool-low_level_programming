6a69fbcb1681ea9ed3b9bde9e63183e1d5a0ac08
Pizzapanda92 <fabio92@sfr.fr>
2024-06-21 10:07:13 +0000

    exo 4

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *
+ *_int _isalpha - function that checks for lowercase character.
+ *
+ * @c: char c
+ *
+ * Return: 0.
+ */
+
+
+int _isalpha(int c)
+
+{
+	if (isalpha(c))
+	{
+		return (1);
+	}
+	return (0);
+}
