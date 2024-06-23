7f4b06a060dd37d098e68bef5dcbf347576049a1
Pizzapanda92 <fabio92@sfr.fr>
2024-06-23 11:24:44 +0000

    exo 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - calcule la somme de deux entiers
+ * @a: premier entier
+ * @b: deuxieme entier
+ *
+ * Return: la somme de a et b
+ */
+
+int add(int a, int b)
+{
+	int sum = a + b;
+	
+	return sum;
+}
---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function that prints the alphabet
+ * in lowercase, followed by a new line 10 times
+ *
+ * Return: void.
+ */
+
+void times_table(void)
+
+{
+	char num = 0;
+
+	while (num <= 10)
+
