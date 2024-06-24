4f9fffa3e818f48ccc79d50d1d257815134bd0f4
Pizzapanda92 <fabio92@sfr.fr>
2024-06-24 11:26:21 +0000

    exo 0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+ /**
+ * _isupper - function that checks for uppercase character
+ *
+ * @c: variavle
+ *
+ * Return: 1 if is Maj , 0 if is min
+ */
+
+int _isupper(int c)
+{
+
+	if (c >= 'A' && c <= 'Z')
+	{
+	return (1);
+	}
+
+		else
+		{
+		return (0);
+		}
+}
---
+++ more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+more fonctions nested loops
