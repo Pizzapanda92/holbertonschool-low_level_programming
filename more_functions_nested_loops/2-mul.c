1509a7871cd0149630db21de60111c9f0070b87f
Pizzapanda92 <fabio92@sfr.fr>
2024-06-24 13:11:32 +0000

    exo 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multipli  la somme de deux entiers
+ * @a: premier entier
+ * @b: deuxieme entier
+ *
+ * Return: la somme de a et b
+ */
+
+int mul(int a, int b)
+{
+	int sum = a * b;
+
+	return (sum);
+}
