27c5904c71e1c2d297c617807ca9fbc627096155
Pizzapanda92 <fabio92@sfr.fr>
2024-06-25 13:08:30 +0000

    exo 9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -16,15 +16,15 @@
 	{
 		if (n % 3 == 0 && n % 5 == 0)
 		{
-			printf(" FizzBuzz ");
+			printf(" FizzBuzz");
 		}
 		else if (n % 3 == 0)
 		{
-			printf(" Fizz ");
+			printf(" Fizz");
 		}
 		else if (n % 5 == 0)
 		{
-			printf(" Buzz ");
+			printf(" Buzz");
 		}
 		else
 		{
@@ -34,4 +34,3 @@
 	printf("\n");
 	return (0);
 }
-
