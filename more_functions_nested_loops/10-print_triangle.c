ace2fe63e0600cb27a72a5c4da2d5b0c0b38cef8
Pizzapanda92 <fabio92@sfr.fr>
2024-06-25 14:23:10 +0000

    exo 10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_triangle - function that prints a triangle, followed by a new line.
+ *
+ * @size: number of #
+ * Return: void.
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+			{
+				if (j <= size - i)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('#');
+				}
+			}
+		_putchar('\n');
+		}
+	}
+}
