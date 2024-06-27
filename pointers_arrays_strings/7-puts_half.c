47ac3589372f132480965a507a8f8e35faff1b80
Pizzapanda <fabio92@sfr.fr>
2024-06-27 12:49:33 +0000

    exo 7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * puts_half - function that reverses a string.
+ *
+ * @str: pointeur to chaine we print
+ *
+ */
+
+void puts_half(char *str)
+{
+	int length;
+	int moit;
+
+	for (length = 0; str[length] != '\0'; length++)
+	{
+
+	}
+	for (moit = 0; moit < length / 2 ; moit++)
+	{
+	}
+
+	for (; moit != length ; moit++)
+	{
+		_putchar (str[moit]);
+	}
+	_putchar ('\n');
+}
