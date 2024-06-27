9eadb541e2b0cccc83aa0f2ed043ef72d89a90e7
Pizzapanda <fabio92@sfr.fr>
2024-06-27 16:07:45 +0000

    exo 8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_array - function that prints n elements of an array of integers,
+ * followed by a new line.
+ *
+ * @a : poiteur
+ * @n : variable
+ */
+
+void print_array(int *a, int n)
+{
+
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+
+		if (i == n - 1)
+		continue;
+		printf(" , ");
+	}
+	printf("\n");
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -3,6 +3,7 @@
 
 #include <unistd.h>
 #include <ctype.h>
+#include <stdio.h>
 
 int _putchar(char c);
 void reset_to_98(int *n);
