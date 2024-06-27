d8f6a915feceaef2d1bbb30f09014f2bac2e7e8a
Pizzapanda <fabio92@sfr.fr>
2024-06-27 19:47:33 +0000

    exo 10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -4,33 +4,34 @@
  *
  * Return: The converted integer.
  */
+
 int _atoi(char *s)
 {
-    int sign = 1;  
-    int result = 0;
-    int digit;
+	int sign = 1;
+	int result = 0;
+	int digit;
 
-    while (*s == ' ' || (*s == '-' && *(s + 1) != '\0') || (*s == '+' && *(s + 1) != '\0'))
-    {
-        if (*s == '-')
-            sign *= -1;
-        s++;
-    }
+while (*s == ' ' || (*s == '-' && *(s + 1) != '\0') || (*s == '+' && *(s + 1) != '\0'))
+	{
+		if (*s == '-')
+			sign *= -1;
+		s++;
+	}
 
-    while (*s >= '0' && *s <= '9')
-    {
-        digit = *s - '0';
+	while (*s >= '0' && *s <= '9')
+	{
+		digit = *s - '0';
 
-        if (result > 214748364 || (result == 214748364 && digit > 7))
-        {
-            if (sign == 1)
-                return 2147483647;
-            else
-                return -2147483648;
-        }
+	if (result > 214748364 || (result == 214748364 && digit > 7))
+		{
+			if (sign == 1)
+				return (2147483647);
+			else
+				return (-2147483648);
+		}
 
-        result = result * 10 + digit;
-        s++;
-    }
-    return result * sign;
+		result = result * 10 + digit;
+		s++;
+	}
+	return (result * sign);
 }
