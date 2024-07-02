4d8a8c407251e3f4ebb06c13a6fdade7a055312c
Pizzapanda92 <fabio92@sfr.fr>
2024-07-02 19:48:43 +0000

    exo 4

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Calcule x élev�la puissance y de manière récursive
+ *
+ * @x: la base
+ * @y: l'exposant
+ *
+ * Return: Le résultat de x^y. Retourne -1 si y est inférieu0.
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0) {
+		return -1;
+	}
+	if (y == 0)
+	{
+		return 1;
+	}
+	return x * _pow_recursion(x, y - 1);
+}
