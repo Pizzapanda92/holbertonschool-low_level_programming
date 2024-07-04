1bcfd370d5b26ba9979bfdeb741e0e4274d7e1cf
Pizzapanda92 <fabio92@sfr.fr>
2024-07-04 14:57:24 +0000

    exo 7 correction

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -15,11 +15,9 @@ void print_chessboard(char (*a)[8])
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
-		{
-			putchar(a[i][j]);
-			if (j < 7)
-				putchar(' ');
-		}
-		putchar('\n');
+		
+			_putchar(a[i][j]);
+		
+		_putchar('\n');
 	}
 }
